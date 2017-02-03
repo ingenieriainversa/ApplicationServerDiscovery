fd6ba269368580518040c5376720ed0aa1b93b12
Ingenieria.inversa <jdg@member.fsf.org>
2017-02-03 13:49:10 +0100

    Update Cell.java

--- src/was/Cell.java
+++ src/was/Cell.java
@@ -1,5 +1,45 @@
 package was;
 
-public class Cell {
+import java.util.ArrayList;
 
+public class Cell {
+	private String cellName;
+	private String cellPath;
+	private String resourcesXml;
+	private ArrayList<Resource> resources;
+
+	/*
+	 * Cell class constructor:
+	 * 
+	 * @cellName: Cell name.
+	 */
+	public Cell(String profilePath, String cellName) {
+		setCellName(cellName);
+		setCellPath(profilePath);
+		setResourcesXml();
+	}
+
+	public String getCellName() {
+		return cellName;
+	}
+
+	public void setCellName(String cellName) {
+		this.cellName = cellName;
+	}
+
+	public String getCellPath() {
+		return cellPath;
+	}
+
+	public void setCellPath(String profilePath) {
+		cellPath = profilePath + "/config/cells/" + cellName;
+	}
+
+	public String getResourcesXml() {
+		return resourcesXml;
+	}
+
+	public void setResourcesXml() {
+		resourcesXml = cellPath + "/resources.xml";
+	}
 }
