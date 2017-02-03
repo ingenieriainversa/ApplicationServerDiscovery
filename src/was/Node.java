4011888fb4fb9e39fac61f12ec47fed1eddff216
Ingenieria.inversa <jdg@member.fsf.org>
2017-02-03 13:50:32 +0100

    Update Node.java

--- src/was/Node.java
+++ src/was/Node.java
@@ -1,5 +1,46 @@
 package was;
 
 public class Node {
+	private String nodeName;
+	private String nodePath;
+	private String resourcesXml;
 
+	/*
+	 * Node class constructor:
+	 * 
+	 * @profilePath: Profile path.
+	 * 
+	 * @cell: Cell object.
+	 * 
+	 * @nodeName: Node name.
+	 */
+	public Node(String profilePath, Cell cell, String nodeName) {
+		setNodeName(nodeName);
+		setNodePath(profilePath, cell);
+		setResourcesXml();
+	}
+
+	public String getNodeName() {
+		return nodeName;
+	}
+
+	public void setNodeName(String nodeName) {
+		this.nodeName = nodeName;
+	}
+	
+	public String getNodePath() {
+		return nodePath;
+	}
+
+	public void setNodePath(String profilePath, Cell cell) {
+		nodePath = profilePath + "/config/cells/" + cell.getCellName() + "nodes/"+ nodeName;
+	}
+	
+	public String getResourcesXml() {
+		return resourcesXml;
+	}
+
+	public void setResourcesXml() {
+		resourcesXml = nodePath +"/resources.xml";
+	}
 }
