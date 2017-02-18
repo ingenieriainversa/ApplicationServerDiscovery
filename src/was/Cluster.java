39b47185b4a770c4daa4d7861eea770fed5fd5f6
ingenieriainversa <jdg@member.fsf.org>
2017-02-18 12:49:54 +0100

    New class Cluster.java

---
+++ src/was/Cluster.java
@@ -0,0 +1,51 @@
+/*
+ * Application Server Discovery v0.01
+ * Cluster.java
+ * Copyleft - 2016  Javier Dominguez Gomez
+ * Written by Javier Dominguez Gomez <jdg@member.fsf.org>
+ * GnuPG Key: 6ECD1616
+ * Madrid, Spain
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package was;
+
+import java.util.ArrayList;
+
+public class Cluster {
+	
+	private String name;
+	private ArrayList<Jvm> jvms;
+	
+	public Cluster(String name) {
+		setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<Jvm> getJvms() {
+		return jvms;
+	}
+
+	public void setJvms(ArrayList<Jvm> jvms) {
+		this.jvms = jvms;
+	}
+}
